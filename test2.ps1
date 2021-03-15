d8d832966ec9daffdfb2fe37a29541655442dd16
He11oW-10 <46309857+He11oW-10@users.noreply.github.com>
2021-03-15 16:48:22 +0800

    Update test2.ps1

--- test2.ps1
+++ test2.ps1
@@ -2,9 +2,9 @@ param(
      [string] $resource_group_name,
      [string] $webapp_name,
      [string] $container_url,
-	   [string] $frequency_int,
+     [string] $frequency_int,
      [string] $frequency_unit,
-		 [string] $utcValue,
-		 [string] $retention_day
-     )
+     [string] $utcValue,
+     [string] $retention_day
+)
 Edit-AzWebAppBackupConfiguration -ResourceGroupName $resource_group_name -Name $webapp_name -StorageAccountUrl $container_url -FrequencyInterval $frequency_int -FrequencyUnit $frequency_unit -StartTime $utcValue -RetentionPeriodInDays $retention_day
